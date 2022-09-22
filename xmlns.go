7b348bf3ed3fbaaeb01653c84cd60b5ffdf08bc1
mingard <info@arthurmingard.com>
2022-09-22 07:57:26 +0100

    fix: use indirect mem reference

--- xmlns.go
+++ xmlns.go
@@ -4,32 +4,32 @@ import "encoding/xml"
 
 var (
 	// TypeDefault is the default XML Namespace type.
-	TypeDefault = xml.Attr{
+	TypeDefault = &xml.Attr{
 		Name:  xml.Name{Local: "xmlns"},
 		Value: "http://www.sitemaps.org/schemas/sitemap/0.9",
 	}
 	// TypeNews is the XML Namespace type for news.
-	TypeNews = xml.Attr{
+	TypeNews = &xml.Attr{
 		Name:  xml.Name{Local: "xmlns:news"},
 		Value: "http://www.google.com/schemas/sitemap-news/0.9",
 	}
 	// TypeXHTML is the XML Namespace type for XHTML.
-	TypeXHTML = xml.Attr{
+	TypeXHTML = &xml.Attr{
 		Name:  xml.Name{Local: "xmlns"},
 		Value: "http://www.w3.org/1999/xhtml",
 	}
 	// TypeMobile is the XML Namespace type for mobile-only content.
-	TypeMobile = xml.Attr{
+	TypeMobile = &xml.Attr{
 		Name:  xml.Name{Local: "xmlns:mobile"},
 		Value: "http://www.google.com/schemas/sitemap-mobile/1.0",
 	}
 	// TypeImage is the XML Namespace type for images.
-	TypeImage = xml.Attr{
+	TypeImage = &xml.Attr{
 		Name:  xml.Name{Local: "xmlns:image"},
 		Value: "http://www.google.com/schemas/sitemap-image/1.1",
 	}
 	// TypeVideo is the XML Namespace type for videos.
-	TypeVideo = xml.Attr{
+	TypeVideo = &xml.Attr{
 		Name:  xml.Name{Local: "xmlns:image"},
 		Value: "http://www.google.com/schemas/sitemap-video/1.1",
 	}
